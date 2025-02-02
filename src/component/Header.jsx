7e7b167825af5096c82d199ae16ea90767f5a417
Shahidul Islam <shahidul13@uttarauniversity.edu.bd>
2025-02-02 11:40:18 +0600

    scss

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,17 +1,33 @@
+import { MoveRight, Search, ShoppingCart } from 'lucide-react'
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 export default function Header() {
-  return (
-    <header>
-        <div className="logo">
-            <h1>Logo</h1>
-        </div>
-        <div className="navbar">
-            <ul>
-                <li><NavLink to="/">Home</NavLink></li>
-            </ul>
-        </div>
-    </header>
-  )
+    return (
+        <header>
+            <div className="mainheader container">
+                <div className="logo">
+                    <img src="/Vector.png" alt="" />
+                </div>
+
+                <div className="navbar">
+                    <ul className='flex flex-cen'>
+                        <li><NavLink to="/">Product</NavLink></li>
+                        <li><NavLink to="/company">services</NavLink></li>
+                        <li><NavLink to="/services">contact up</NavLink></li>
+                    </ul>
+                </div>
+
+                <div className="buttons flex flex-cen">
+                    <div className="icones flex flex-cen">
+                        <Search />
+                        <ShoppingCart />
+                    </div>
+                    <button className="btn">Login</button>
+                    <button className="btn">Sign Up</button>
+                </div>
+
+            </div>
+        </header>
+    )
 }

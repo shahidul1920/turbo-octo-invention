24b2a6ed0cba75b267ca92c48fb4d4e7bda82d6a
Shahidul Islam <shahidul13@uttarauniversity.edu.bd>
2025-02-06 16:54:09 +0600

    all product page done

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PopProducts from '../component/PopProducts'
 
 export default function ProductPage() {
+
+    const [allProducts, setAllProducts] = useState([])
+
+    useEffect(() => {
+        fetch('/allProduct.json')
+            .then(res => res.json())
+            .then(json => setAllProducts(json))
+    }, [])
+    
+
   return (
-    <div>
+    <div className='allProductPage'>
         <div className="popularProdct">
             <PopProducts />
         </div>
 
-      <div className="container">
-        <h1>hola</h1>
+      <div className="container mainProducts">
+        {allProducts.map((p)=>(
+            <div className="prod" key={p.id}>
+                <div className="img">
+                    <img src={p.image} alt="" />
+                </div>
+                <div className="text">
+                    <div className="title">
+                        <h4>{p.title}</h4>
+                        <div className="rating flex flex-cn">
+                            <img src="/star.png" alt="" />
+                            <p>{p.rating}</p>
+                        </div>
+                    </div>
+                    <div className="priceNnane flex flex-cn">
+                        <h2>$ {p.price}</h2>
+                        <h5>{p.name} <span>per KG</span></h5>
+                    </div>
+                    <div className="button">
+                        <button className='btn btn-p'>Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+        ))}
       </div>
     </div>
   )
